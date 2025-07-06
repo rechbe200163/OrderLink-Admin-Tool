3b09c3fc26aa1f7c41ef6195a4dbfb644ee19e0d
Michael Rechberger <71632874+rechbe200163@users.noreply.github.com>
2025-07-06 10:56:20 +0200

    refactor: consolidate site configuration logic and remove unused service

--- admin-tool/lib/actions/siteConfig.actions.ts
+++ admin-tool/lib/actions/siteConfig.actions.ts
@@ -3,19 +3,17 @@
 import { cookies } from 'next/headers';
 import { FormState } from '../form.types';
 import { redirect } from 'next/navigation';
-import { hasPermission } from '../utlis/getSession';
-import { siteConfigService } from '../services/SiteConfigService';
+import { getSession, hasPermission } from '../utlis/getSession';
 import { stripeService } from '../services/StripeService';
 import { apiPut } from './api.actions';
 import { ENDPOINTS } from '../api/endpoints';
-import prisma from '@/prisma/client';
 
 export async function updateSiteConfig(
   siteConfigId: string,
   _prevState: FormState,
   formData: FormData
 ): Promise<FormState> {
-  const session = await auth();
+  const session = await getSession();
 
   if (!session) {
     return {
@@ -44,42 +42,4 @@ export async function updateSiteConfig(
   } catch (error: any) {
     return { success: false, errors: { title: [error.message] } };
   }
-}
-
-export async function createSiteConfig(
-  _prevState: FormState,
-  formData: FormData
-): Promise<FormState> {
-  const resp = await siteConfigService.createSiteConfig(formData);
-
-  if (!resp.success) {
-    return resp;
-  }
-
-  // Ensure siteConfig exists
-  const siteConfig = await prisma.siteConfig.findUnique({
-    where: {
-      siteConfigId: resp.data + '',
-    },
-  });
-  if (!siteConfig) {
-    return {
-      success: false,
-      errors: {
-        title: [
-          'Connecting to stripe account failed, you can try again after the onboarding process is complete',
-        ],
-      },
-    };
-  }
-
-  const stripeAccountId = await stripeService.createConnectAccount(
-    siteConfig.email
-  );
-  await siteConfigService.addStripeAccountId(stripeAccountId);
-
-  const cookieStore = await cookies();
-  cookieStore.set('isSiteConfigured', 'true', { maxAge: 60 * 60 * 24 * 7 });
-
-  redirect('/onboarding/initialAdmin');
 }
