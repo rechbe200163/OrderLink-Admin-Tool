154b63b27d9d01ab7f487a3c9cbc97ec40c32127
Michael Rechberger <71632874+rechbe200163@users.noreply.github.com>
2025-07-16 20:06:41 +0200

    feat: add pagination to customer and product selects

--- admin-tool/app/(admin-panel)/orders/add/page.tsx
+++ admin-tool/app/(admin-panel)/orders/add/page.tsx
@@ -1,14 +1,10 @@
 import CreateOrder from '@/components/helpers/orders/CreateOrder';
-import { customerApiService } from '@/lib/api/concrete/customers';
-import { productApiService } from '@/lib/api/concrete/products';
 import React from 'react';
 
 async function CreateOrdersPage() {
-  const products = await productApiService.getAll();
-  const customers = await customerApiService.getAll();
   return (
     <div className='min-w-full p-5'>
-      <CreateOrder customer={customers} products={products} />
+      <CreateOrder />
     </div>
   );
 }
